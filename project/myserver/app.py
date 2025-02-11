8f828e274034346a89223d2bb9e9329b6cbf9b46
Raja Ramesh Haddubangi <h.rajaramesh@gmail.com>
2025-02-11 17:57:02 +0530

    task chain added

--- project/myserver/app.py
+++ project/myserver/app.py
@@ -4,29 +4,17 @@ from pymongo import MongoClient
 import random
 import time
 import os
-from worker import process_row
-
+import json
+from worker import start_task_chain, record_task
 
 app = FastAPI()
 
-
-
-def record_task(tid):
-    mongo = MongoClient("mongodb://mongodbserver:27017/")
-    tdb = mongo["test"]
-    tasks = tdb["tasks"]
-    res = tasks.insert_one({"task_id":tid})
-    return "task initiated"
-
-def processData():
-    fname = "data/countries.csv"
-    with open(fname, 'r') as cf:
-        creader = csv.reader(cf)
-        for row in creader:
-            rnd = random.randint(10,100)
-            task = process_row.delay(row, rnd)
-            record_task(task.id)
-
+def processData(fname):    
+    with open(fname, 'r') as f:
+        pages = json.load(f)
+        rnd = random.randint(10,100)
+        task = start_task_chain.delay(pages, rnd)
+        record_task(task.id)
 
 @app.get("/")
 def home(request:Request):
@@ -34,5 +22,6 @@ def home(request:Request):
 
 @app.get("/task")
 def task(bgtask:BackgroundTasks, request:Request):
-    tid = processData()
+    fname = "sample_pages.json"
+    tid = processData(f"data/{fname}")
     return "background task added"
--- project/myserver/worker.py
+++ project/myserver/worker.py
@@ -1,3 +1,4 @@
+import random
 from celery import Celery
 import time
 from pymongo import MongoClient
@@ -7,13 +8,33 @@ celery = Celery(__name__)
 celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://redis:6379")
 celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://redis:6379")
 
-
-@celery.task(name="process_row")
-def process_row(r, s):
-    time.sleep(s) # simulates a long task which takes random time
+def record_task(tid):
     mongo = MongoClient("mongodb://mongodbserver:27017/")
     tdb = mongo["test"]
-    ctry = tdb["countries"]
-    d = {"country":r[0],"latitude":r[1],"longitude":r[2],"name":r[3]}
-    res = ctry.insert_one(d)
-    return res.inserted_id == None
+    tasks = tdb["tasks"]
+    res = tasks.insert_one({"task_id":tid})
+    return f"task {tid} initiated"
+
+@celery.task(name="start_task_chain")
+def start_task_chain(pages, s):
+    #starting tasks
+    time.sleep(s) # sim time taken to start task chain
+    task = ct_group_pages.delay(pages,s)
+    return record_task(task.id)
+
+@celery.task(name="ct_group_pages")
+def ct_group_pages(pages, s):
+    time.sleep(s) # sim time taken to group pages
+    groups = [[1,2],[3],[4,5],[6,7],[8,9,10]]
+    # return groups
+    for group in groups:
+        rnd = random.randint(10,100)
+        task = ct_process_group.delay(group,rnd)
+        record_task(task.id)
+
+
+@celery.task(name="ct_process_group")
+def ct_process_group(group, s):
+    time.sleep(s) # sim time taken to process group
+    ents = { "in" : "1234", "id" : "01/01/2025", "ia" : 10.0, "group":group }
+    return ents
